11ad8c02d4ba39d89ba81eb104fd416b09724139
Isabelle Ferreira <isabelle.ferreira3000@gmail.com>
2019-05-27 15:11:30 -0300

    error: negative dimensions

--- lab9/lenet5.py
+++ lab9/lenet5.py
@@ -6,23 +6,32 @@ def make_lenet5():
     model = Sequential()
 
     # Todo: implement LeNet-5 model
-    # ENTRADA:
-    model.add(layers.Conv2D(filters=1, input_shape=(32, 32, 1)))
-
     # 1A CAMADA:
-    model.add(layers.Conv2D(filters=6, kernel_size=(5, 5), strides=1, activation=activations.tanh))
+    model.add(layers.Conv2D(filters=6,
+                            kernel_size=(5, 5),
+                            strides=(1, 1),
+                            activation=activations.tanh,
+                            input_shape=(32, 32, 1)))
 
     # 2A CAMADA:
-    model.add(layers.AveragePooling2D(pool_size=(px, py), strides=2))
+    model.add(layers.AveragePooling2D(pool_size=(6, 6),
+                                      strides=(2, 2)))
 
     # 3A CAMADA:
-    model.add(layers.Conv2D(filters=16, kernel_size=(5, 5), strides=1, activation=activations.tanh))
+    model.add(layers.Conv2D(filters=16,
+                            kernel_size=(5, 5),
+                            strides=(1, 1),
+                            activation=activations.tanh))
 
     # 4A CAMADA:
-    model.add(layers.AveragePooling2D(pool_size=(px, py), strides=2))
+    model.add(layers.AveragePooling2D(pool_size=(16, 16),
+                                      strides=(2, 2)))
 
     # 5A CAMADA:
-    model.add(layers.Conv2D(filters=120, kernel_size=(5, 5), strides=1, activation=activations.tanh))
+    model.add(layers.Conv2D(filters=120,
+                            kernel_size=(5, 5),
+                            strides=(1, 1),
+                            activation=activations.tanh))
 
     # TRANSICAO:
     model.add(layers.Flatten())
